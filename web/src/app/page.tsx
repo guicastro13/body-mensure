9912e69a3a63064b83a2edd7f3ead7c8e8f50708
Guilherme Castro <guii_1@hotmail.com>
2024-04-07 15:05:37 -0300

    add 2 features, range input to intensity, and now, we have a logic to repair muscle

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,44 @@
-'use client'
-import { useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import WomanBody from "./components/woman-body";
 
-const muscleColors = {
+type MuscleColors = {
+  trapezio: number;
+  deltoidLareal: number;
+  deltoidAnterior: number;
+  deltoidPosterior: number;
+  romboide: number;
+  peitoralMaior: number;
+  tricepesLogo: number;
+  tricepesMedio: number;
+  tricepesLateral: number;
+  latissimoDorso: number;
+  bicipes: number;
+  serratil: number;
+  absMedio: number;
+  absInfra: number;
+  absLateral: number;
+  lombar: number;
+  gluteoMaximo: number;
+  gluteoMedio: number;
+  sartori: number;
+  adutor: number;
+  retoFemoral: number;
+  vastoMedial: number;
+  vastoLaretal: number;
+  bicipesFemoral: number;
+  semitendineo: number;
+  semiMembranoso: number;
+  panturilhaMedial: number;
+  panturilhaLateral: number;
+};
+
+interface Exercise {
+  nome: string;
+  callback: () => void;
+}
+
+const muscleColors: MuscleColors = {
   trapezio: 0,
   deltoidLareal: 0,
   deltoidAnterior: 0,
@@ -104,21 +140,31 @@ const ativacaoMuscular = {
   panturrilhaEmPe: {
     panturilhaMedial: 80,
     panturilhaLateral: 70,
-  }
+  },
 };
 
-export default function Home() {
-  const [exercicioSelecionado, setExercicioSelecionado] = useState("");
-  const [body, setBody] = useState(muscleColors)
+const Home: React.FC = () => {
+  const [exercicioSelecionado, setExercicioSelecionado] =
+    useState<string>("Supino");
+  const [body, setBody] = useState<MuscleColors>({ ...muscleColors });
+  const [intensidade, setIntensidade] = useState<number>(100);
+  const [exerciciosDiarios, setExerciciosDiarios] = useState<Exercise[]>([]);
 
-  const exercicios = [
+  const handleChangeIntensidade = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setIntensidade(parseInt(event.target.value)); // Atualiza o estado da intensidade
+  };
+
+  const exercicios: Exercise[] = [
     {
       nome: "Supino",
       callback: () => {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["supino"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -129,7 +175,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["puxadaFrontal"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -140,7 +187,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["agachamento"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -151,7 +199,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["levantamentoTerra"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -162,7 +211,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["desenvolvimentoComHalteres"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -173,7 +223,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["barraFixa"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -184,7 +235,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["roscaDireta"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -195,7 +247,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["elevacaoLateral"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -206,7 +259,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["extensaoDeTriceps"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -217,7 +271,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["abdominais"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -228,7 +283,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["stiff"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -239,7 +295,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["afundo"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -250,7 +307,8 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["flexaoDePernas"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
@@ -261,49 +319,119 @@ export default function Home() {
         const novosValores: any = { ...body };
         const ativacao: any = ativacaoMuscular["panturrilhaEmPe"];
         for (const musculo in ativacao) {
-          novosValores[musculo] = ativacao[musculo];
+          novosValores[musculo] += ativacao[musculo] * (intensidade / 100);
+          if (novosValores[musculo] > 100) novosValores[musculo] = 100;
         }
         setBody(novosValores);
       },
-    }
+    },
   ];
+
+  const handleAdicionarExercicio = () => {
+    const exercicioEncontrado = exercicios.find(
+      (exercicio) => exercicio.nome === exercicioSelecionado
+    );
+    if (exercicioEncontrado) {
+      exercicioEncontrado.callback();
+      const novoBody: any = { ...body };
+      const ativacao: any =
+        ativacaoMuscular[exercicioSelecionado as keyof typeof ativacaoMuscular];
+      for (const musculo in novoBody) {
+        if (Object.prototype.hasOwnProperty.call(novoBody, musculo)) {
+          novoBody[musculo] += novoBody[musculo] +=
+            ativacao[musculo] * (intensidade / 100);
+          if (novoBody[musculo] > 100) novoBody[musculo] = 100;
+        }
+      }
+      setBody(novoBody);
+      setExerciciosDiarios((prevExercicios) => [
+        ...prevExercicios,
+        exercicioEncontrado,
+      ]);
+      console.log(body);
+    }
+  };
+
+  useEffect(() => {
+    const reduzirDanoMuscularDiario = () => {
+      setBody((prevBody) => {
+        console.log(prevBody);
+        const novoBody: any = { ...prevBody };
+        for (const musculo in novoBody) {
+          let porcentagemAtual = novoBody[musculo];
+          porcentagemAtual -= 10;
+          if (porcentagemAtual < 0) {
+            novoBody[musculo] = 0;
+          } else if (porcentagemAtual > 100) {
+            novoBody[musculo] = 100;
+          } else {
+            console.log(
+              "REDUZIR " +
+                musculo +
+                ",de " +
+                novoBody[musculo] +
+                " para " +
+                porcentagemAtual
+            );
+            novoBody[musculo] = porcentagemAtual;
+          }
+        }
+        return novoBody;
+      });
+    };
+
+    const reducaoDiariaInterval = setInterval(() => {
+      reduzirDanoMuscularDiario();
+    }, 2000);
+
+    return () => clearInterval(reducaoDiariaInterval);
+  }, []);
+
   return (
-<>
-  <div className="bg-white p-4">
-    <WomanBody muscleColors={body}/>
-  </div>
-  <div className="bg-slate-600 p-4 flex items-center justify-between">
-    <button
-      className="bg-gray-500 text-white rounded-md px-4 py-2 mr-2"
-      onClick={() => setBody(muscleColors)}
-    >
-      Restaurar Padrões
-    </button>
-    <select
-      id="exercicio"
-      name="Exercicio"
-      className="bg-white text-gray-900 rounded-md p-2 mr-2"
-      onChange={(e) => setExercicioSelecionado(e.target.value)}
-    >
-      {exercicios.map((exercicio) => (
-        <option key={exercicio.nome} value={exercicio.nome}>
-          {exercicio.nome}
-        </option>
-      ))}
-    </select>
-    <button
-      className="bg-blue-500 text-white rounded-md px-4 py-2"
-      onClick={() => {
-        const exercicioEncontrado = exercicios.find(
-          (exercicio) => exercicio.nome === exercicioSelecionado
-        );
-        if (!exercicioEncontrado) return
-        exercicioEncontrado.callback();
-      }}
-    >
-      Adicionar
-    </button>
-  </div>
-</>
+    <>
+      <div className="bg-white p-4">
+        <WomanBody muscleColors={body} />
+      </div>
+      <div className="bg-slate-600 p-4 flex items-center justify-between">
+        <button
+          className="bg-gray-500 text-white rounded-md px-4 py-2 mr-2"
+          onClick={() => setBody(muscleColors)}
+        >
+          Restaurar Padrões
+        </button>
+        <select
+          id="exercicio"
+          name="Exercicio"
+          className="bg-white text-gray-900 rounded-md p-2 mr-2"
+          onChange={(e) => setExercicioSelecionado(e.target.value)}
+        >
+          {exercicios.map((exercicio) => (
+            <option key={exercicio.nome} value={exercicio.nome}>
+              {exercicio.nome}
+            </option>
+          ))}
+        </select>
+        <button
+          className="bg-blue-500 text-white rounded-md px-4 py-2"
+          onClick={handleAdicionarExercicio}
+        >
+          Adicionar
+        </button>
+        <div>
+          <label htmlFor="intensidade">Intensidade:</label>
+          <input
+            type="range"
+            id="intensidade"
+            name="intensidade"
+            min="0"
+            max="100"
+            value={intensidade}
+            onChange={handleChangeIntensidade}
+          />
+          <span>{intensidade}</span>
+        </div>
+      </div>
+    </>
   );
-}
+};
+export default Home;
